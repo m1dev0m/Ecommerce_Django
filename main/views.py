863e868aa17b40dc5d718613451168e78114e8b3
Maqsat <github1maqsat@gmail.com>
2025-02-18 20:55:05 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 def home(request):
     return render(request, 'main/home.html')
+
+
+def some_view(request):
+    data = {"message": "Hello, world!"}
+    return JsonResponse(data)
